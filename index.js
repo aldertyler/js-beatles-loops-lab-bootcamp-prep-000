495a4941fe596ad9140ce6c003fe9b89929296f6
Tyler Alder <tyler.b.alder@gmail.com>
2018-05-30 22:54:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
+  var emptyArray = [];
+  
+  for (var i = 0; i < arrayOfMusicians.length; i ++ ) {
+    emptyArray.push(`${arrayOfMusicians[i]} plays ${arrayOfInstruments[i]}`);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(arrayOfFacts) {
+  var emptyArray = [];
+  let i = 0;
+  while (i < emptyArray.length) {
+    emptyArray.push(`${arrayOfFacts[i]} !!! `);
+    i++;
+    }
+    return emptyArray;
+}
